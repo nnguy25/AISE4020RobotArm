40a0cd03246326966edc1fe91d1d69d3b4e43909
Aaron Triguero <122829984+ThunderFlame2230@users.noreply.github.com>
2025-01-29 18:30:21 +0000

    test

--- robot_test.py
+++ robot_test.py
@@ -1,1 +1,25 @@
+from pymycobot.mycobot import MyCobot
+
+from pymycobot import PI_PORT, PI_BAUD
 import time
+
+# Create object code here for Raspberry Pi version
+mc = MyCobot(PI_PORT, PI_BAUD)
+
+
+# Check whether the program can be burned into the robot arm
+if mc.is_controller_connected() != 1:
+    print("Please connect the robot arm correctly for program writing")
+    exit(0)
+
+
+i = 7
+#loop 7 times
+while i > 0:                            
+    mc.set_color(0,0,255) #blue light on
+    time.sleep(2)    #wait for 2 seconds                
+    mc.set_color(255,0,0) #red light on
+    time.sleep(2)    #wait for 2 seconds
+    mc.set_color(0,255,0) #green light on
+    time.sleep(2)    #wait for 2 seconds
+    i -= 1
