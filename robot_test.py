d6b8c512cfad684bcfcf7839a4fa6f5558358171
Aaron Triguero <122829984+ThunderFlame2230@users.noreply.github.com>
2025-03-05 15:17:41 -0500

    Update robot_test.py
    Updated to incorporate multiple inputs

--- robot_test.py
+++ robot_test.py
@@ -1,5 +1,6 @@
 from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
+from pymycobot.genre import Coord
 
 from time import sleep
 from enum import Enum
@@ -25,13 +26,22 @@ print("Raspberry Pi is ready to receive messages.")
 """
 ======Global Variables======
 """
-# Positions
+# Positions)
+
 NEUTRAL_POS_COORDS = [57.5, -64.4, 408.6, -92.37, 0.17, -89.83]
-BIN_A_COORDS = [103.6, 75.2, 402.2, -88.15, 1.71, -24.46]
-BIN_B_COORDS = [46.0, 67.6, 407.3, -87.93, 11.96, 18.44]
-BIN_C_COORDS = [105.3, -110.7, 349.8, -83.04, 16.89, -112.61]
-BIN_D_COORDS = [73.1, -124.9, 393.2, -92.03, 6.24, -124.85]
-LOADING_ZONE_COORDS = [119.8, 33.7, 380.3, -81.99, 3.74, -43.88]
+READY_POS_COORDS = [-67.7, 7.1, 410.7, -79.26, 26.97, 179.05]
+BIN_A_COORDS =[84.8, 34.3, 414.8, -75.34, 24.26, -14.66]
+BIN_B_COORDS = [52.7, 51.6, 417.1, -80.18, 25.12, 18.27]
+BIN_C_COORDS = [39.3, -88.8, 352.2, -73.65, 14.38, -110.75]
+BIN_D_COORDS = [-4.0, -78.4, 421.0, -72.89, -0.95, -127.55]
+LOADING_ZONE_COORDS = [84.2, -9.4, 329.7, -63.81, 22.36, -35.01]
+
+#Quadrants
+Q1 = [130.2, -52.1, 382.4, -85.64, -8.24, -84.2]# bot right
+Q2 = [142.1, 19.9, 380.7, -85.0, -12.9, -50.92]# bot left
+Q3 = [148.7, -56.4, 385.5, -80.31, -11.8, -86.59]# top right
+Q4 = [162.7, -1.5, 382.2, -81.22, -4.13, -64.02]# top left
+
 # Set speed
 SPEED = 30
 
@@ -45,7 +55,7 @@ class PumpStatus(Enum):
     """
     OFF = 1
     ON = 0
-
+ 
 def pump_on():
     """
     Turns on the suction pump.
@@ -63,6 +73,7 @@ def pump_off():
     sleep(0.5)
     #GPIO.output(20, PumpStatus.OFF.value)
     #GPIO.output(20, PumpStatus.ON.value)
+    
     #sleep(0.05)
     GPIO.output(20, PumpStatus.OFF.value)
 
@@ -92,21 +103,44 @@ def move_to_bin(bin):
         
        
     
-def pickup_object(letter):
+def pickup_object(quadrant):
     # [ move to object ]
-    mc.release_servo(3)
-    mc.release_servo(4)
-    input("Hold onto robot arm. Press any key to continue")
-    mc.release_all_servos()     # manually release to desired object   
-    input("Place robot mouth onto object. Press any key to continue")
+    #mc.release_servo(3)
+    #mc.release_servo(4)
+    #input("Hold onto robot arm. Press any key to continue")
+    #mc.release_all_servos()     # manually release to desired object   
+    #input("Place robot mouth onto object. Press any key to continue")
+    #pump_on()
+    #sleep(2)
+    #neutral_pos()
+    
+    # Travel to quadrant
+    if quadrant=='Q1':
+        mc.set_color(255,0,0) #red light on
+        mc.sync_send_coords(coords=Q1,speed=SPEED,timeout=3)
+    elif quadrant=='Q2':
+        mc.set_color(255,255,0) #yellow light on
+        mc.sync_send_coords(coords=Q2,speed=SPEED,timeout=3)
+    elif quadrant=='Q3':
+        mc.set_color(0,255,0) #green light on
+        mc.sync_send_coords(coords=Q3,speed=SPEED,timeout=3)
+    elif quadrant=='Q4':
+        mc.set_color(0,0,255) #blue light on
+        mc.sync_send_coords(coords=Q4,speed=SPEED,timeout=3)
+    # turn pump on
     pump_on()
     sleep(2)
-    neutral_pos()
+    ready_pos()
+    
 
 def neutral_pos():
     # Return to neutral position
     mc.send_coords(coords=NEUTRAL_POS_COORDS, speed=SPEED)
     #mc.sync_send_coords(coords=NEUTRAL_POS_COORDS,speed=SPEED)
+ 
+def ready_pos():
+    # Return to ready position
+    mc.send_coords(coords=READY_POS_COORDS, speed=SPEED)
 
 
 
@@ -124,13 +158,25 @@ def main(received):
 
 
     # [ Get input for letter (via hand gesture) and bin]
-    LETTER = 'center'#input("Input letter: ")
-    BIN = received
-    print("Inputs:",BIN,LETTER)
+    QUAD = received
+    
+    #BIN = received
+    print("Item in quadrant:",QUAD)
     
     # [ If object seen starts with [letter], pick up ]
     print("Picking up object")
-    pickup_object(LETTER)
+    pickup_object(QUAD)
+
+    # Another receival
+    print("Waiting for BIN input...")
+    new_received = received
+    while (new_received == received) | (new_received == ''):
+            new_received = ser.readline().decode().strip()  # Read incoming data
+    BIN = new_received
+    print(type(BIN))
+    print("Placing item in Bin:",BIN)
+    sleep(0.5)
+
 
     # [ Pick up object, move to bin, drop object in bin]
     print("\nMoving to Bin", BIN)
@@ -140,7 +186,7 @@ def main(received):
     print("Releasing block")
     pump_off()
     mc.set_color(128,128,128) #  reset to grey color
-    sleep(1)
+    sleep(1) 
     
     print("Returning to base position")
     neutral_pos()
@@ -173,6 +219,7 @@ if __name__ == "__main__":
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(channel=20, direction=GPIO.OUT)
         GPIO.setup(channel=21, direction=GPIO.OUT)
+        pump_off()
         print("waiting for input")
         while True:
             received = ser.readline().decode().strip()  # Read incoming data
