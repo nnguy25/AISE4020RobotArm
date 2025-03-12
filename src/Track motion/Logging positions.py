0e2883a88d23a07a02f99a234d1703c5244e1de5
JoshuaCamillo <122766591+JoshuaCamillo@users.noreply.github.com>
2025-03-12 15:25:15 -0400

    Moving robot with preset coordinates

--- src/Track motion/Logging positions.py
+++ src/Track motion/Logging positions.py
@@ -2,20 +2,25 @@ from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
 import time
 
-# Replace with your serial port (e.g., /dev/ttyUSB0 or COM3 on Windows)
+# Create object code here for Raspberry Pi version
 mc: MyCobot = MyCobot(port=PI_PORT, baudrate=str(PI_BAUD))
 
-# Enable free mode to move the robot manually
-mc.set_free_mode(1)
+# Check whether the program can be burned into the robot arm
+if mc.is_controller_connected() != 1:
+    print("Please connect the robot arm correctly for program writing")
+    exit(0)
+
 print("Move the robot manually to trace the path...")
 
 positions = []
 NEUTRAL_POS_COORDS = [-31.7, -51.5, 410.7, -74.87, -3.65, -73.34]
-mc.send_angles(NEUTRAL_POS_COORDS, 50)  # 50% speed
+mc.send_coords(coords=NEUTRAL_POS_COORDS, speed=30)
 
-time.sleep(10)
+mc.release_all_servos()
 
 #make it wait to start until input
+
+name = input("Name File for quadrant:")
 
 input("Press Enter to start recording...")
 
@@ -23,25 +28,26 @@ try:
     while True:
         # Read and store the current joint angles
         joints = mc.get_angles()
-        print("Current angles:", joints)
         positions.append(joints)
-        time.sleep(0.2)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("Recording stopped.")
-    mc.set_free_mode(0)  # Disable free mode
+    mc.send_coords(coords=NEUTRAL_POS_COORDS, speed=30)
     #move to the initial position
-
-    mc.send_angles(NEUTRAL_POS_COORDS, 50)  # 50% speed
+    #export to txt file
+    with open(name, "w") as f:
+        for pos in positions:
+            f.write(str(pos) + "\n")
     
 
 
-time.sleep(10)
-print("Replaying recorded motion...")
+time.sleep(5)
+print("Replaying recorded motion smoothly...")
 
 for pos in positions:
-    mc.send_angles(pos, 50)  # 50% speed
-    time.sleep(0.2)
+    mc.sync_send_angles(pos, 80)  # Faster and smoother motion
 
 print("Motion complete!")
+
 mc.release_all_servos()   
